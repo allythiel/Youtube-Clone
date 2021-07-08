fde1f0f5ff75d3ebb429eeb9da86cee1a99cb44f
Craig Satchell <satchdev@gmail.com>
2021-07-08 19:32:56 -0400

    fix schema exports

--- models/comment.js
+++ models/comment.js
@@ -2,43 +2,43 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const replySchema = new mongoose.Schema({
-    text: { type: String, required: true, minLength: 5, maxLength: 1000 }, 
-    dateAdded: { type: Date, default: Date.now },
+   text: { type: String, required: true, minLength: 5, maxLength: 1000 },
+   dateAdded: { type: Date, default: Date.now },
 });
 
 const commentSchema = new mongoose.Schema({
-    text: { type: String, required: true, minLength: 5, maxLength: 1000 },
-    dateAdded: { type: Date, default: Date.now },
-    likes: { type: Number, required: true, default: 0 },
-    dislikes: { type: Number, required: true, default: 0 },
-    reply: [{ type: replySchema }],
-    videoId: { type: String, required: true, minLength: 4, maxLength: 50 },
+   text: { type: String, required: true, minLength: 5, maxLength: 1000 },
+   dateAdded: { type: Date, default: Date.now },
+   likes: { type: Number, required: true, default: 0 },
+   dislikes: { type: Number, required: true, default: 0 },
+   reply: [{ type: replySchema }],
+   videoId: { type: String, required: true, minLength: 4, maxLength: 50 },
 });
 
 const Reply = mongoose.model('Reply', replySchema);
 const Comment = mongoose.model('Comment', commentSchema);
 
 function validateComment(comment) {
-    const schema = Joi.object({
-        text: Joi.string().min(5).max(1000).required(),
-        likes: Joi.number().required(),
-        dislikes: Joi.number().required(),
-        videoId: Joi.string().min(4).max(50).required(),
-    });
-    return schema.validate(comment);
+   const schema = Joi.object({
+      text: Joi.string().min(5).max(1000).required(),
+      likes: Joi.number().required(),
+      dislikes: Joi.number().required(),
+      videoId: Joi.string().min(4).max(50).required(),
+   });
+   return schema.validate(comment);
 }
 
 function validateReply(reply) {
-    const schema = Joi.object({
-        text: Joi.string().min(5).max(1000).required(),
-    });
-    return schema.validate(reply);
+   const schema = Joi.object({
+      text: Joi.string().min(5).max(1000).required(),
+   });
+   return schema.validate(reply);
 }
 
 module.exports = {
-    Reply: Reply,
-    Comment: Comment,
+   Reply: Reply,
+   Comment: Comment,
 };
 
-exports.validate = validateComment;
-exports.validate = validateReply;
+exports.validateComment = validateComment;
+exports.validateReply = validateReply;
