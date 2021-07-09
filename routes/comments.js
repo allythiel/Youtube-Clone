f813307d0504074dcc278f15eed13bf825f8414a
Craig Satchell <satchdev@gmail.com>
2021-07-09 00:25:27 -0400

    compl. & tested API routes

--- routes/comments.js
+++ routes/comments.js
@@ -29,7 +29,7 @@ router.get('/:videoId', async (req, res) => {
 });
 
 
-// create new comment
+// Post new comment
 router.post('/', async (req, res) => {
    try {
       // Need to validate body before continuing
@@ -52,18 +52,30 @@ router.post('/', async (req, res) => {
 });
 
 
-// // reply to comment by video id
-// router.post('/:videoId/comment/:commentId', async (req, res) => {
-//    try {
-//       const comment = await Comment.find({ videoId: req.params.videoId });
-//       console.log(comment);
-//       if (!comment)
-//          return res.status(400).send(`The comment with id "${req.params.videoId}" d
-//    oes not exist.`);
-//       return res.send(comment);
-//    } catch (ex) {
-//       return res.status(500).send(`Internal Server Error: ${ex}`);
-//    }
-// });
+// Post reply to comment
+router.post('/:commentId', async (req, res) => {
+   try {
+      const comment = await Comment.findById(req.params.commentId);
+
+      const { error } = validateReply(req.body);
+      if (error)
+         return res.status(400).send(error);
+
+      if (!comment)
+         return res.status(400).send(`The comment with id "${req.params.commentId}" d
+   oes not exist.`);
+
+      const reply = new Reply({
+         text: req.body.text,
+      });
+
+      comment.replies.push(reply);
+      await comment.save();
+
+      return res.send(comment);
+   } catch (ex) {
+      return res.status(500).send(`Internal Server Error: ${ex}`);
+   }
+});
 
 module.exports = router;
